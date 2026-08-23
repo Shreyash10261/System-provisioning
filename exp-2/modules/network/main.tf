c7bf20c936bf5c70d577b6f875ced15a9705d491
Shreyash10261 <shivhare.shreyash10@gmail.com>
2026-08-24 00:38:50 +0530

    Fix terraform fmt alignment issues

--- exp-2/modules/network/main.tf
+++ exp-2/modules/network/main.tf
@@ -17,9 +17,9 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.availability_zone
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.availability_zone
   # tfsec:ignore:aws-ec2-no-public-ip-subnet
   map_public_ip_on_launch = true
 
